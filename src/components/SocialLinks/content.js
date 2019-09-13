747c43507aba2518ef49a4caef0b9957c059159a
Julio Melo <julio@alfamultimidia.com.br>
2019-09-13 14:49:02 -0300

    social links icons

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/juliovmelo',
+  },
+  {
+    label: 'Twitter',
+    url: 'https://google.com',
+  },
+  {
+    label: 'Youtube',
+    url: 'https://google.com',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://google.com',
+  },
+]
+
+export default links
